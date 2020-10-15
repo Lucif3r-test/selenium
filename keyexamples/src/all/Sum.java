a4e2fbb4edde553d7c47e479a99fe658f3ad7a82
Biswajit Pradhan <Lucif3r-test@users.noreply.github.com>
2020-10-15 22:00:11 +0530

    sum commit

--- keyexamples/src/all/Sum.java
+++ keyexamples/src/all/Sum.java
@@ -1,9 +1,20 @@
 package all;
 
+import java.util.Scanner;
+
 public class Sum {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		// to sum two numbers
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number");
+		int x=sc.nextInt();
+		System.out.println("enter a number");
+		int y=sc.nextInt();
+		//to sum
+		int z=x+y;
+		System.out.println("sum="+ z);
 
 	}
 

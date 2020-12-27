b140db835e4b80e8ee610c90388c623a760d0a32
Biswajit Pradhan <Lucif3r-test@users.noreply.github.com>
2020-12-27 23:21:26 +0530

    minor changes main

---
+++ keyexamples/src/all/ArraySorting1ton.java
@@ -0,0 +1,38 @@
+package all;
+
+public class ArraySorting1ton 
+{
+	// function for sort array 
+    static void sortit(int []arr, int n) 
+    { 
+        for (int i = 0; i < n; i++)  
+        { 
+            arr[i]=i+1;     
+        } 
+        System.out.println(arr);
+    } 
+      
+    // Driver code 
+    public static void main(String args[]) 
+    { 
+        int []arr = {10, 7, 9, 2, 8,  
+                            3, 5, 4, 6, 1}; 
+        int n = arr.length; 
+      
+        // for sort an array 
+        sortit(arr, n); 
+      
+        // for print all the  
+        // element in sorted way 
+        for (int i = 0; i < n; i++)  
+        {
+        	System.out.print(arr[i] + " ");
+        }
+              
+    } 
+			
+			
+
+}
+
+

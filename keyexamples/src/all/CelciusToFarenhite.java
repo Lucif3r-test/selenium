8040b6029f8840816e2edcb7557d64f107f07293
Biswajit Pradhan <Lucif3r-test@users.noreply.github.com>
2021-01-02 22:52:38 +0530

    new changes

---
+++ keyexamples/src/all/CelciusToFarenhite.java
@@ -0,0 +1,21 @@
+package all;
+
+public class CelciusToFarenhite {
+
+	public static void main(String[] args) 
+	{
+		int a = 21;    
+		double a1 =  9/5* a + 32; 
+		System.out.println( a1);
+		double b = 21.0;  
+		double a2 =  (9/5)* b + 32;      
+		System.out.println( a2);
+		double c = 21.0;  double a3=  9*c/5 + 32;        System.out.println( a3);
+		double d = 21.0;  double a4=  9.*(d/5.0) + 32;   System.out.println( a4);
+		double e = 21.0;  double a5=  9.0* e/5.0 + 32;    System.out.println( a5);
+		int f = 21;    double a6=  9* f/5 + 32;        System.out.println( a6);
+		double g = 21.0;  double a7=  (1/5)*9*g + 32;    System.out.println( a7);
+		int h = 21;    double a8=  (1./5)*9*h + 32;   System.out.println( a8);
+	}
+
+}
